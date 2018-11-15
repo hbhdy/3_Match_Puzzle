a73d64de280aaa3e10e452cf79e55eea7d1105f3
DESKTOP-MS94ACF\shtke <shtkeh77@gmail.com>
2018-11-15 23:09:06 +0900

    3 Match - Hexagon Random Create - Hexagon Move

---
+++ 3_Match_Puzzle/Assets/02.Script/BoardCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCtrl : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    public GameObject tilePrefab;
+    private BackgroundCtrl[,] allTiles;
+
+    public GameObject[] hexagons;
+
+    public GameObject[,] allHexagons;
+
+    // Use this for initialization
+    void Start()
+    {
+        allTiles = new BackgroundCtrl[width, height];
+        allHexagons = new GameObject[width, height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 startPosition = new Vector2(i, j);  // 초기 위치를 잡아준다.
+                GameObject backgroundTile = Instantiate(tilePrefab, startPosition, Quaternion.identity) as GameObject; // 생성할때 값 설정
+                backgroundTile.transform.parent = this.transform; // 그후 Board 오브젝트에 넣기
+                backgroundTile.name = "(" + i + "," + j + ") Tile";
+
+                int useToHexagon = Random.Range(0, hexagons.Length);
+                GameObject hexagon = Instantiate(hexagons[useToHexagon], startPosition, Quaternion.identity);
+                hexagon.transform.parent = this.transform;
+                hexagon.name = "(" + i + "," + j + ") Hexagon";
+                allHexagons[i, j] = hexagon;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ 3_Match_Puzzle/Assets/02.Script/HexagonCtrl.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonCtrl : MonoBehaviour
+{
+    public int col;
+    public int row;
+    public int targetX;
+    public int targetY;
+
+    private BoardCtrl board;
+    private GameObject otherHexagon;
+
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+    private Vector2 tempPosition;
+    public float swipeAngle = 0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        board = FindObjectOfType<BoardCtrl>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        col = targetX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetX = col;
+        targetY = row;
+        if (Mathf.Abs(targetX - transform.position.x) > .1)
+        {
+            // Move Toward the target
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .1f);
+        }
+        else
+        {
+            //Directly set the position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allHexagons[col, row] = this.gameObject;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            // Move Toward the target
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .1f);
+        }
+        else
+        {
+            //Directly set the position
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allHexagons[col, row] = this.gameObject;
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        startTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(startTouchPosition);
+    }
+
+    private void OnMouseUp()
+    {
+        endTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+
+        swipeAngle = Mathf.Atan2(endTouchPosition.y - startTouchPosition.y, endTouchPosition.x - startTouchPosition.x) * 180 / Mathf.PI;
+        //Debug.Log(swipeAngle);
+        MoveHexagonPieces();
+    }
+
+    void MoveHexagonPieces()
+    {
+        // Right 방향으로 움직임
+        if (swipeAngle > -45 && swipeAngle <= 45 && col < board.width)
+        {
+            otherHexagon = board.allHexagons[col + 1, row];
+            otherHexagon.GetComponent<HexagonCtrl>().col -= 1;
+            col += 1;
+        }
+        //  방향으로 움직임
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        {
+            otherHexagon = board.allHexagons[col, row + 1];
+            otherHexagon.GetComponent<HexagonCtrl>().row -= 1;
+            row += 1;
+        }
+        //  방향으로 움직임
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && col >0)
+        {
+            otherHexagon = board.allHexagons[col -1, row];
+            otherHexagon.GetComponent<HexagonCtrl>().col += 1;
+            col -= 1;
+        }
+        //  방향으로 움직임
+        else if (swipeAngle >= -135 && swipeAngle < -45 && row > 0)
+        {
+            otherHexagon = board.allHexagons[col, row-1];
+            otherHexagon.GetComponent<HexagonCtrl>().row+= 1;
+            row -= 1;
+        }
+    }
+}
